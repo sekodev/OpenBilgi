b676a6fb0e8484b66e153f8aa1ff6104370b74a0
Serkan Aksit <5222368+sekodev@users.noreply.github.com>
2022-05-31 02:32:33 +0300

    Add build & config files

---
+++ config.lua
@@ -0,0 +1,31 @@
+local resWidth, resHeight
+
+if ( string.sub( system.getInfo("model"), 1, 4 ) == "iPad" ) then
+    resWidth = 768
+    resHeight = 1024
+elseif ( display.pixelHeight / display.pixelWidth > 2 ) then
+    resWidth = 810
+    resHeight = 1800
+elseif ( display.pixelHeight / display.pixelWidth > 1.72 ) then
+    resWidth = 720
+    resHeight = 1280
+else
+    resWidth = 768
+    resHeight = 1280
+end
+
+application =
+{
+    content =
+    {
+        width = resWidth,
+        height = resHeight,
+        fps = 60,
+
+        imageSuffix =
+        {
+        	["@2x"] = 1.5,
+            ["@4x"] = 3,
+        },
+    },
+}
