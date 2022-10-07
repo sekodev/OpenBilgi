812a908f347ac94c27c5e8184adf4c7d72ebb266
Serkan Aksit <5222368+sekodev@users.noreply.github.com>
2022-10-07 16:12:58 +0300

    Update version information

--- main.lua
+++ main.lua
@@ -52,7 +52,7 @@ local function assignVariables()
     composer.setVariable( "userName" , "asd" )
     composer.setVariable( "userToken", "" )
 
-    composer.setVariable( "currentVersion" , "50 (0.7.1)" ) -- Visible in Settings screen
+    composer.setVariable( "currentVersion" , "OpenBilgi, 52 (0.7.2)" ) -- Visible in Settings screen
     composer.setVariable( "packageName" , "io.sleepybug.bilgi" ) -- Required to show rating pop-ups
     composer.setVariable( "urlGooglePlay" , "https://play.google.com/store/apps/details?id=io.sleepybug.bilgi" ) -- Required for sharing store link on social media
     --composer.setVariable( "urlAppStore" , "https://play.google.com/store/apps/details?id=io.sleepybug.bilgi" ) -- Will be used for App Store version
