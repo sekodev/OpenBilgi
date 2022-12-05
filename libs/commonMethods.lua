6d9e53b7e0c5718d729a647dae2b25bc7837e97d
Serkan Aksit <5222368+sekodev@users.noreply.github.com>
2022-12-05 21:55:07 +0300

    Create commonMethods library
    This file gathers project specific common methods that are used in different screens

---
+++ libs/commonMethods.lua
@@ -0,0 +1,38 @@
+------------------------------------------------------------------------------
+--
+-- This file is part of OpenBilgi, a roguelike trivia game repository
+--
+-- For overview and more information on licensing please refer to README.md 
+--
+-- Home page: https://github.com/sekodev/OpenBilgi
+--
+-- Contact: info.sleepybug@gmail.com
+--
+------------------------------------------------------------------------------
+
+-- This file gathers project specific common methods that are used in different screens
+-- Using those methods in other projects will probably require significant modifications
+
+local commonMethods = {}
+
+function commonMethods.showLocksAvailable(targetGroup, yTopInfoBox, locksAvailable, fontLockText)
+    local widthUIButton = contentWidthSafe / 9
+    local heightUIButton = widthUIButton
+
+    local imageLock = display.newImageRect( targetGroup, "assets/menu/padlock.png", widthUIButton, heightUIButton )
+    imageLock:setFillColor( unpack(themeData.colorPadlock) )
+    imageLock.x = display.contentCenterX - imageLock.width / 2
+    imageLock.y = yTopInfoBox - imageLock.height
+
+    local fontSizeCurrency = imageLock.height / 1.1
+
+    local optionsNumLocks = { text = locksAvailable, font = fontLockText, fontSize = fontSizeCurrency }
+    imageLock.textNumAvailable = display.newText( optionsNumLocks )
+    imageLock.textNumAvailable:setFillColor( unpack(themeData.colorTextDefault) )
+    imageLock.textNumAvailable.x = imageLock.x + imageLock.width / 2 + imageLock.textNumAvailable.width
+    imageLock.textNumAvailable.y = imageLock.y
+    targetGroup:insert(imageLock.textNumAvailable)
+end
+
+
+return commonMethods
